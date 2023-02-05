f465c03106570488ba7fd8320a3c45495f366d3b
Casey McCullough <casey.mccullough@techelevator.com>
2023-02-05 06:50:29 -0700

    set coin images to show for each flip

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const FLIP_STRING_MAX = 20;
 const TARGET_1 = 'TTH';
 const TARGET_2 = 'HTT';
+
+const headsImg = 'img/244px_Quarter_Heads.png';
+const tailsImg = '../img/240_257_Quarter_Tails.png';
+
+let coinImage = document.getElementById("coin-image");
 let flipListElement = document.getElementById("flipList");
 let currentFlipElement = document.getElementById("current-flip");
 let playerOneSet = document.getElementById("player-1-set");
@@ -13,6 +18,11 @@ let currentFlip = '';
 function flip() {
   currentFlip = getFlip();
   flipList += currentFlip;
+  if (currentFlip === 'H'){
+    coinImage.src = headsImg;
+  } else {
+    coinImage.src = tailsImg;
+  }
   updateHTML();
   count++;
 }
