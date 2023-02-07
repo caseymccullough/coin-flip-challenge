3b11a68134c1c7f6fbe66b65c40f17ff24d56815
Casey McCullough <casey.mccullough@techelevator.com>
2023-02-07 06:28:30 -0700

    displays the wining set of each player as coins. Identifies win in console

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const FLIP_STRING_MAX = 20;
-const TARGET_1 = 'TTH';
-const TARGET_2 = 'HTT';
+const PLAYER_SET_1 = 'TTH';
+const PLAYER_SET_2 = 'HTT';
 
 const headsImg = 'img/244px_Quarter_Heads.png';
 const tailsImg = '../img/240_257_Quarter_Tails.png';
@@ -12,29 +12,33 @@ let playerOneSet = document.getElementById("player-1-set");
 let playerTwoSet = document.getElementById("player-2-set");
 
 let count = 0;
-let flipList = '';
+let flipString = '';
 let currentFlip = '';
 
 function flip() {
   currentFlip = getFlip();
-  addFlipToList(currentFlip);
-  if (currentFlip === 'H'){
-    coinImage.src = headsImg;
-  } else {
-    coinImage.src = tailsImg;
-  }
+  flipString += currentFlip;
+  let lastThree = flipString.substring(flipString.length - 3);
+  console.log(flipString + '\t' + lastThree);
+  checkForWin(lastThree);
   updateHTML();
-  count++;
+  
 }
 
-function addFlipToList(flip) {
-  let newCoin = document.createElement("span");
-  newCoin.textContent = flip;
-  flipListArchive.appendChild(newCoin);
-  newCoin.classList.add("coin");
-  let classToAdd = flip === 'H' ? 'heads' : 'tails';
-  newCoin.classList.add(classToAdd);
+function displayCoinList(flip) {
 
+  const newCoin = getCoinElement(flip);
+  flipListArchive.appendChild(newCoin);
+  
+}
+
+function getCoinElement(coinLabel){
+  let newCoin = document.createElement("span");
+  newCoin.textContent = coinLabel;
+  newCoin.classList.add("coin");
+  let classToAdd = coinLabel === 'H' ? 'heads' : 'tails';
+  newCoin.classList.add(classToAdd);
+  return newCoin;
 }
 
 function getFlip() {
@@ -43,16 +47,46 @@ function getFlip() {
   return headOrTail;
 }
 
+function displayCurrentFlip(currentFlip){
+  if (currentFlip === 'H'){
+    coinImage.src = headsImg;
+  } else {
+    coinImage.src = tailsImg;
+  }
+}
+
+function addPlayerSets(){
+  playerOneSet.replaceChildren();
+  playerTwoSet.replaceChildren();
+  for (let i = 0; i < PLAYER_SET_1.length; i++){
+    const coin = getCoinElement(PLAYER_SET_1.charAt(i));
+    playerOneSet.appendChild(coin);
+  }
+
+  for (let i = 0; i < PLAYER_SET_2.length; i++){
+    const coin = getCoinElement(PLAYER_SET_2.charAt(i));
+    playerTwoSet.appendChild(coin);
+  }
+}
+
+function checkForWin(lastThreeFlips){
+  if (lastThreeFlips === PLAYER_SET_1){
+    console.log("\nPlayer One Wins!")
+  }else if (lastThreeFlips === PLAYER_SET_2){
+    console.log("\Player Two Wins!");
+  }
+}
+
 function restart() {
-  flipList = '';
+  flipString = '';
   currentFlip = '';
-  playerOneSet.innerHTML = TARGET_1;
-  playerTwoSet.innerHTML = TARGET_2;
+  addPlayerSets();
   flipListArchive.replaceChildren(); //with no parameter removes all child elements
   updateHTML();
 }
 
 function updateHTML(){
-
+  displayCurrentFlip(currentFlip);
+  displayCoinList(currentFlip);
   
 }
